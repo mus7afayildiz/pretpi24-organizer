aa9c71b8f9d3e943c65b3b3c1c9448408dc0b931
unknown <mus7afayildiz@yandex.com>
2025-03-19 14:31:41 +0100

    Login avec laravel Breeze

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
-use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $heureServeur = now();
-    $resultatSQL = DB::select("SELECT NOW() AS heure_db");
-    $heureDB = $resultatSQL[0]->heure_db ?? 'Non disponible';
-    
-    //$resultatSQL = DB::select("SELECT 1 as result FROM dual");
-    //$resultatSQL = DB::raw("SELECT NOW()")->getValue(DB::connection()->getPdo());
-    //$resultatSQL = DB::scalar("SELECT NOW()");
-    
-    return view('index', compact('heureServeur', 'heureDB'));
+    return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
