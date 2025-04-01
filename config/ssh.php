e3db103e03690e5ed2f38dac7528933cb655cd4d
unknown <mus7afayildiz@yandex.com>
2025-04-01 15:22:02 +0200

    feat: déploiement avec github action

---
+++ config/ssh.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'host' => env('SSH_HOST', 'organizer.w3.pm2etml.ch'),
+    'username' => env('SSH_USERNAME', 'mustafa-ssh'),
+    'private_key_path' => env('SSH_PRIVATE_KEY_PATH', '/path/to/privatekey'),
+];
